d66327593a0eaad2f8e36e05f40795bc89094b53
sneubig <neubig.stephen@gmail.com>
2015-01-05 11:37:31 -0500

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :welcome, only: :index
+  resources :businesses, only: [:index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
