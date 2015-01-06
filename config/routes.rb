4eb7f529682df094c3b0f83f3904493d4118574b
sneubig <neubig.stephen@gmail.com>
2015-01-05 23:03:07 -0500

    removed welcome and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'bars#index'
-  resources :bars
   # resources :welcome, only: :index
   post '/search' => 'bars#search'
 
